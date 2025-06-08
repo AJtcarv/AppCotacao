99524bd1e06ffdce25b08c2d6c40007819158295
AJtcarv <164242015+AJtcarv@users.noreply.github.com>
2025-06-08 12:42:56 -0300

    IntegrandoApiCotacao

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+import requests
 
 GUI = Builder.load_file("tela.kv")
 
@@ -8,9 +9,18 @@ class MeuAplicativo(App):
         return GUI
     
     def on_start(self):
-        self.root.ids["moeda1"].text = "Dolar R$ 6"
-        self.root.ids["moeda2"].text = "Euro R$ 7"
-        self.root.ids["moeda3"].text = "BitCoin R$ 35k"
-        self.root.ids["moeda4"].text = "Ethereum R$"
+        self.root.ids["moeda1"].text = f"Dolar R${self.pegar_cotacao('USD')}"
+        self.root.ids["moeda2"].text = f"Euro R${self.pegar_cotacao('EUR')}"
+        self.root.ids["moeda3"].text = f"BitCoin R${self.pegar_cotacao('BTC')}"
+        self.root.ids["moeda4"].text = f"Ethereum R${self.pegar_cotacao('ETH')}"
+
+    
+    def pegar_cotacao(self, moeda):
+        link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
+        requisicao = requests.get(link)
+        dic_requisicao = requisicao.json()
+        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
+        return cotacao
+
 
 MeuAplicativo().run()
