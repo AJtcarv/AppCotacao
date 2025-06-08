673a5a7b2d9bb42f38d9940ec99862b7d00f298b
AJtcarv <164242015+AJtcarv@users.noreply.github.com>
2025-06-08 12:17:26 -0300

    AjusteNoArquivoKv

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ class MeuAplicativo(App):
         return GUI
     
     def on_start(self):
-        self.root.ids["moeda1"] = "Dolar R$"
-        self.root.ids["moeda2"] = "Euro R$"
-        self.root.ids["moeda3"] = "BitCoin R$"
+        self.root.ids["moeda1"] = "Dolar R$ 6"
+        self.root.ids["moeda2"] = "Euro R$ 7"
+        self.root.ids["moeda3"] = "BitCoin R$ 35k"
         self.root.ids["moeda4"] = "Ethereum R$"
 
 MeuAplicativo().run()
